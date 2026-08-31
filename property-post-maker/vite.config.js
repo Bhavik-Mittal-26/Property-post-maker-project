aca302fef1d73e944d72d798edf5182df9630e6c
Bhavik Mittal <26bmittal@gmail.com>
2026-08-31 14:41:42 +0530

    updated

--- property-post-maker/vite.config.js
+++ property-post-maker/vite.config.js
@@ -4,4 +4,15 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
 })
